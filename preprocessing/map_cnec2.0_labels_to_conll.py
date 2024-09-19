5b9a54f3880b8f7b392b17ccbc24b798c563b10c
Jana Strakova <strakova@ufal.mff.cuni.cz>
2024-09-19 13:53:04 +0200

    http -> https

--- preprocessing/map_cnec2.0_labels_to_conll.py
+++ preprocessing/map_cnec2.0_labels_to_conll.py
@@ -6,7 +6,7 @@
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 """Script to harmonize CNEC 2.0 with CoNLL-2003 PER, ORG, LOC, MISC labels.
 
