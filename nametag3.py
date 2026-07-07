0c45cd9047208abc399dc3c9ec65c9602dc5c548
Jana Strakova <strakova@ufal.mff.cuni.cz>
2026-07-07 13:57:49 +0200

    Bump version from 3.2.0 to 3.3.0-dev.

--- nametag3.py
+++ nametag3.py
@@ -93,7 +93,7 @@ from nametag3_dataset import NameTag3Dataset
 from nametag3_dataset_collection import NameTag3DatasetCollection
 from nametag3_model import nametag3_model_factory
 
-__version__ = "3.2.0"
+__version__ = "3.3.0-dev"
 
 
 if __name__ == "__main__":
